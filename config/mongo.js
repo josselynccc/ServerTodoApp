6f30244a1151263cb84ae8d7b2519ed2df43d594
Josselyn Cortez <cristheljosselyn1@gmail.com>
2023-12-14 10:57:18 -0500

    deploy1

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnect = () => {
-    const DB_URI = 'mongodb://127.0.0.1:27017';
+    const DB_URI = 'mongodb+srv://cristheljosselyn1:p08xaZBkQLm791aI@cluster0.6sjyqr8.mongodb.net/';
     mongoose.connect(DB_URI)
         .then(() => {
             console.log('**** CONEXIÓN EXITOSA ****');
